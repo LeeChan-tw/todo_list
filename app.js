4ca112ee84ffbbbeb0ba697ae57eb7cc0913837c
LeeChan <lee79102@hotmail.com>
2020-10-11 18:05:47 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express')
 //執行express()，得到一個伺服器
 const app = express()
 const port = 3000
+
 const mongoose =require('mongoose')
 mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const Todo = require('./models/todo') //  載入 Todo model
+
+const exphbs = require('express-handlebars')
+
 //取得資料庫連線狀態
 const db = mongoose.connection
+
 //連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -16,14 +23,17 @@ db.once('open', () => {
 })
 
 //啟用Handlebars
-const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 //設定路由
 //Todo 首頁
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', (req, res) => {    
+  Todo.find() // 取出 Todo model 裡面所有資料
+    .lean() // 把 Mongoose 的 model 物件轉會成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos})) // 將資料傳到 index 樣板
+    .catch(error => console.error(error))
+  
 })
 
 app.listen(port, () => {
